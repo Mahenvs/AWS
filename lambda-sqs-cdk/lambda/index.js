ff6c2c7ad52a6b63e2c65bd70f28107b4782729c
mahenv <mahendrauppari044@gmail.com>
2025-05-02 17:21:18 -0500

    adding layer to lambda

--- lambda-sqs-cdk/lambda/index.js
+++ lambda-sqs-cdk/lambda/index.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 exports.handler = async (event) => {
   console.log("Hello, this is serverless lambda function");
   for(const {messageId,body} of event.Records){
@@ -5,6 +7,10 @@ exports.handler = async (event) => {
     console.log("I am in message quwu");
     
   }
+  console.log('Received SQS event:', JSON.stringify(event, null, 2));
+
+  const response = await axios.get('https://api.github.com');
+  console.log('GitHub API status:', response.status);
   return {
     status: 200,
     message:
--- lambda-sqs-cdk/lib/lambda-sqs-cdk-stack.ts
+++ lambda-sqs-cdk/lib/lambda-sqs-cdk-stack.ts
@@ -3,16 +3,24 @@ import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as eventSources from "aws-cdk-lib/aws-lambda-event-sources";
+import * as path from "path";
 
 export class LambdaSqsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create axios layer
+    const axiosLayer = new lambda.LayerVersion(this, "AxiosLayer", {
+      code: lambda.Code.fromAsset(path.join(__dirname, "../layers")),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      description: "Reusable layer with axios",
+    });
     // The code that defines your stack goes here
     const fn = new lambda.Function(this, "LambdaFunctionFromCDK", {
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: "index.handler",
       code: lambda.Code.fromAsset("lambda"),
+      layers: [axiosLayer],
     });
     // example resource
     const queue = new sqs.Queue(this, "LambdaSqsCdkQueue", {
