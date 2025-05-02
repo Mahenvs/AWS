e40dd202614ef541951608c8a9806d408b125225
mahenv <mahendrauppari044@gmail.com>
2025-05-02 16:50:02 -0500

    integrated simple queue service with lambda

--- lambda-sqs-cdk/lambda/index.js
+++ lambda-sqs-cdk/lambda/index.js
@@ -1,5 +1,10 @@
-exports.handler = async () => {
+exports.handler = async (event) => {
   console.log("Hello, this is serverless lambda function");
+  for(const {messageId,body} of event.Records){
+    console.log(messageId,body)
+    console.log("I am in message quwu");
+    
+  }
   return {
     status: 200,
     message:
--- lambda-sqs-cdk/lib/lambda-sqs-cdk-stack.ts
+++ lambda-sqs-cdk/lib/lambda-sqs-cdk-stack.ts
@@ -2,13 +2,14 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as eventSources from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class LambdaSqsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    new lambda.Function(this, "LambdaFunctionFromCDK", {
+    const fn = new lambda.Function(this, "LambdaFunctionFromCDK", {
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: "index.handler",
       code: lambda.Code.fromAsset("lambda"),
@@ -17,5 +18,7 @@ export class LambdaSqsCdkStack extends cdk.Stack {
     const queue = new sqs.Queue(this, "LambdaSqsCdkQueue", {
       visibilityTimeout: cdk.Duration.seconds(300),
     });
+
+    fn.addEventSource(new eventSources.SqsEventSource(queue));
   }
 }
