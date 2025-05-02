ceb11f507f073b9fe01d3dd158c224528e77856c
mahenv <mahendrauppari044@gmail.com>
2025-05-02 16:41:23 -0500

    adding sqs service

--- lambda-sqs-cdk/lib/lambda-sqs-cdk-stack.ts
+++ lambda-sqs-cdk/lib/lambda-sqs-cdk-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqs from "aws-cdk-lib/aws-sqs";
 
 export class LambdaSqsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,8 +14,8 @@ export class LambdaSqsCdkStack extends cdk.Stack {
       code: lambda.Code.fromAsset("lambda"),
     });
     // example resource
-    // const queue = new sqs.Queue(this, 'LambdaSqsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const queue = new sqs.Queue(this, "LambdaSqsCdkQueue", {
+      visibilityTimeout: cdk.Duration.seconds(300),
+    });
   }
 }
